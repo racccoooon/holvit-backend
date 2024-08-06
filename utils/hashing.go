5c70a9b2498876e83ddba93dfcd605a5e80511c9
the127 <julian.baehr@googlemail.com>
2024-08-06 19:35:00 +0200

    fix bug for passwords longer than 72 bytes

--- utils/hashing.go
+++ utils/hashing.go
@@ -1,7 +1,7 @@
 package utils
 
 import (
-	"crypto/sha256"
+	"crypto/sha512"
 	"fmt"
 	"github.com/go-crypt/crypt"
 	"golang.org/x/crypto/bcrypt"
@@ -16,16 +16,9 @@ type BCryptHashAlgorithm struct {
 	Cost int
 }
 
-type UnsupportedHashAlgorithmError struct {
-	Algorithm string
-}
-
-func (e *UnsupportedHashAlgorithmError) Error() string {
-	return fmt.Sprintf("Unsupported hash algorithm: %s", e.Algorithm)
-}
-
 func CompareHash(plain string, hash string) error {
-	valid, err := crypt.CheckPassword(plain, hash)
+	sha := CheapHash(plain)
+	valid, err := crypt.CheckPassword(sha, hash)
 	if err != nil {
 		return err
 	}
@@ -36,7 +29,8 @@ func CompareHash(plain string, hash string) error {
 }
 
 func (b *BCryptHashAlgorithm) Hash(plain string) (string, error) {
-	hashBytes, err := bcrypt.GenerateFromPassword([]byte(plain), b.Cost)
+	sha := CheapHash(plain)
+	hashBytes, err := bcrypt.GenerateFromPassword([]byte(sha), b.Cost)
 	if err != nil {
 		return "", err
 	}
@@ -44,15 +38,8 @@ func (b *BCryptHashAlgorithm) Hash(plain string) (string, error) {
 }
 
 func CheapHash(input string) string {
-	// Create a new SHA-256 hash
-	hash := sha256.New()
-
-	// Write the input data to the hash
+	hash := sha512.New()
 	hash.Write([]byte(input))
-
-	// Calculate the SHA-256 hash and get the result as a byte slice
 	hashedData := hash.Sum(nil)
-
-	// Convert the byte slice to a hexadecimal string
 	return fmt.Sprintf("%x", hashedData)
 }
